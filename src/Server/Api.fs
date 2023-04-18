fb4431183e54297fad619706991e4a2e7d14f73c
[mash ahmed] <mash.ahmed@uk.qbe.com>
2023-04-18 16:21:27 +0100

    Multiple file uploads

--- src/Server/Api.fs
+++ src/Server/Api.fs
@@ -62,4 +62,5 @@ let dojoApi = {
     GetCrimes = getCrimeReport  //fun postcode -> async { return Array.empty }
 
 (* Task 4.2 WEATHER: Hook up the weather endpoint to the getWeather function. *)
+    GetWeather = getWeather
 }
